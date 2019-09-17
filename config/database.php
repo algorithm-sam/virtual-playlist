dcf4f7f3ae6e30668dddae83e8e076b2b1f97e1a
unknown <oluwatobisamuel2020@gmail.com>
2019-09-17 17:58:38 +0100

    fixed the db host issue for cleardb

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', $url["host"]),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', substr($url["path"], 1)),
             'username' => env('DB_USERNAME', $url["user"]),
